48eea7511b66e3359624e681f3bfaee3f163bb19
panchaliSam <panchalisam20030117@gmail.com>
2024-09-06 10:41:59 +0530

    Remove router routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 // import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import {MultiLevelSidebar} from './components/sideNavBar';
 
 function App() {
